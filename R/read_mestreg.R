03b9fc27d51ee3ca93d2c7cc45541c69460b8dee
Alessandro Gasparini <alessandro.gasparini@reddooranalytics.se>
2023-08-22 15:12:21 +0300

    Added function to read estimation results from Stata

---
+++ R/read_mestreg.R
@@ -0,0 +1,26 @@
+#' @title Read Estimation Results From Stata
+#'
+#' @param path_eb Path of .xlsx file containing `e(b)` estimation results as
+#'     exported by `putexcel` in Stata.
+#' @param path_eV Path of .xlsx file containing `e(V)` estimation results as
+#'     exported by `putexcel` in Stata.
+#'
+#' @return A list with two elements, one for `e(b)` (named `eb`) and one for `e(V)`
+#'     (named `eV`).
+#' @export
+#'
+read_e <- function(path_eb, path_eV) {
+  # Read e(b)
+  tbl <- readxl::read_excel(path = path_eb, skip = 1)
+  tbl$`...1` <- NULL
+  eb <- as.numeric(tbl[1, ])
+  names(eb) <- names(tbl)
+  # Read e(V)
+  eV <- readxl::read_excel(path = path_eV, col_names = FALSE)
+  eV <- as.matrix(eV)
+  colnames(eV) <- names(eb)
+  rownames(eV) <- names(eb)
+  # Return
+  out <- list(eb = eb, eV = eV)
+  return(out)
+}
